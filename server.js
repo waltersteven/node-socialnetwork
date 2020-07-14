74a1ade762e57f38f7b2e5eda681f28749fa3bf0
Walter Pariona <wltrst97@gmail.com>
2020-07-14 00:17:15 -0500

    Life is good

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => res.send('API running'));
+
+// Server listen config
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
