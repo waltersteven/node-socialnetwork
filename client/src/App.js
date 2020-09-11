118a45b638901c713dbfb6b2ec8180c817fbb09a
Walter Pariona <wltrst97@gmail.com>
2020-09-11 01:08:50 -0500

    Added action to get all posts and save it in the state

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,7 @@ import AddExperience from './components/profile-forms/AddExperience';
 import AddEducation from './components/profile-forms/AddEducation';
 import Profiles from './components/profiles/Profiles';
 import Profile from './components/profile/Profile';
+import Posts from './components/posts/Posts';
 //
 import PrivateRoute from './components/routing/PrivateRoute';
 // Redux
@@ -49,6 +50,7 @@ function App() {
               <PrivateRoute exact path='/edit-profile' component={EditProfile}></PrivateRoute>
               <PrivateRoute exact path='/add-experience' component={AddExperience}></PrivateRoute>
               <PrivateRoute exact path='/add-education' component={AddEducation}></PrivateRoute>
+              <PrivateRoute exact path='/posts' component={Posts}></PrivateRoute>
             </Switch>
           </section>
         </Fragment>
